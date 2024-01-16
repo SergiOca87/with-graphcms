14521d47739e6db5f5ae50c553b2d6a8f9645abc
ocasergi@gmail.com <ocasergi@gmail.com>
2024-01-16 09:15:45 +0100

    cart context

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 // pages/_app.js
 import { Box, Flex, ChakraProvider } from '@chakra-ui/react';
+import CartContext from '../lib/context/Cart';
+import { useState } from 'react';
+import NavBar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
+	const [items, setItems] = useState({});
+
 	return (
 		<ChakraProvider>
-			<Flex w="full" minH="100vh" bgColor="gray.100">
-				<Box maxW="70vw" mx="auto">
-					<Component {...pageProps} />
-				</Box>
-			</Flex>
+			<CartContext.Provider value={{ items, setItems }}>
+				<Flex w="full" minH="100vh" bgColor="gray.100">
+					<NavBar />
+					<Box maxW="70vw" mx="auto">
+						<Component {...pageProps} />
+					</Box>
+				</Flex>
+			</CartContext.Provider>
 		</ChakraProvider>
 	);
 }
